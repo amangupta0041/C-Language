c5b0842441878355539ffa347cb276dcae17e5ee
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 10:18:05 +0530

    Add files via upload

---
+++ lec2.3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+{ 
+   printf("-\t-\t-\t\n\n");
+   printf("|\t \t|\n\n");
+   printf("A\t \t|\n\n");
+   printf("M\t \t|\n\n");
+   printf("A\t \t|\n\n");
+   printf("N\t \t|\n\n");
+   printf("|\t \t|\n\n");
+   printf("-\t-\t-\t\n\n");
+   
+   
+}
