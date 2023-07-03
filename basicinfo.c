8d3bf7eb71ae6f6833a0d3c944ad838ef9fa015f
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 10:14:14 +0530

    Add files via upload

---
+++ basicinfo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+
+{
+	printf("Name : Aman Gupta\t\n");
+	printf("Father Name:Anup Gupta\t\n");
+	printf("Mother Name:Anita Gupta\t\n");
+	printf("Address : 271, Sai Dham Society, Near Vrundavan Park, Godadara,Surat\t\n");
+	printf("D.O.B :   18-08-2006\t\n");
+	printf("Mobile No.:9157773677\t\n");
+	printf("Gmail : amangupta7243@gmail.com\t\n");
+	
+}
