71feb22cb4c33e2d02c7005479836403def49bc2
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 10:15:33 +0530

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+ {  
+    //g = 9.8 , pi = 3.14 
+     
+    const float g = 9.8;
+    const float pi= 3.14;
+    printf("%.1f\n",g);
+    printf("%.2f",pi);
+ }
