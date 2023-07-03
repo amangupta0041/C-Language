92c7357e607d383d58f250bed3a6afa0bb888816
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 10:24:37 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+ main()
+ 
+ {   
+     int a,b,c ;
+  
+     printf("Enter value of a :");
+	 scanf("%d", &a);
+	 printf("Enter value of b :");
+	 scanf("%d", &b);
+	 
+	 c = a;
+	 a = b;
+	 b = c;
+	 printf("a : %d\n" , a);
+	 printf("b : %d", b);  
+      
+ }
+ 
