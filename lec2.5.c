474f944131903d094e1c1fc09d8280fd3086bd12
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 10:20:41 +0530

    Add files via upload

---
+++ lec2.5.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main() 
+ 
+ {                                                                
+ 
+    printf("*\t\n*\t\n*\t\n*\t\n*       *  *\t\t\n*     *       *\t\n*    *\t\t*\t\n*   *  \t\t* *\t\n*    ");
+    
+    
+ }
