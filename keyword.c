63392bfe1dade173a528ce47c84a0b12f720c8aa
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 10:16:49 +0530

    Add files via upload

---
+++ keyword.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main()
+
+{ 
+    //for +,-,*,/,%
+ 
+    int a = 100;
+    int b = 100;
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+}
