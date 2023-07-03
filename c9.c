3ed3f1955041d65db8f947dd8b3867c108d1ca72
amangupta0041 <138423460+amangupta0041@users.noreply.github.com>
2023-07-03 09:37:05 +0530

    Add files via upload

---
+++ c9.c
@@ -0,0 +1,21 @@
+#include<stdio.h> // header file or liabrary , standard input output
+#include<conio.h> // console input output .h -> header file
+
+main() // <- main function
+{	// body start
+	
+	
+	//datatype variableName;   <- variable declartion
+	//datatype variableName = value; <- variable initialization
+	
+	int age = 18;
+	float perc;
+	age = 19;
+	
+	printf("My name is Akhil\n");
+	printf("my %d age : ",age);
+	
+	
+}	// body over
+
+
